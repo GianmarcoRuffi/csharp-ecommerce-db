30036036808e75ce567abbb60ee373bd008d9aa8
GianmarcoRuffi <gianmarco.ruffi@outlook.it>
2022-11-16 16:29:35 +0100

    Prova aggiunta dati

--- Program.cs
+++ Program.cs
@@ -1,5 +1,19 @@
-﻿public class Program
-{
+﻿using System.Linq;
 
-    static void Main() {}
+static void Main() {}
+
+
+using (EcommerceContext db = new EcommerceContext())
+{
+    // Create
+    Products newProduct = new Products { Name = "Fucile LR300", Description = "Stalkera i tuoi avversari con questo fucile di grado militare.", Price = 900 };
+    db.Add(newProduct);
+    db.SaveChanges();
+    // Read
+    Console.WriteLine("Recupero lista di Prodotti");
+    List<Products> products = db.Products.OrderBy(products => products.Name).ToList();
 }
+
+
+
+
